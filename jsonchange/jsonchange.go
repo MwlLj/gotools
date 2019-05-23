db4a2dbc60ed1b47257d11799188714699d28165
MwlLj <731025894@qq.com>
2019-05-23 13:50:36 +0800

    通过 io.Reader 转换

--- jsonchange/jsonchange.go
+++ jsonchange/jsonchange.go
@@ -2,6 +2,8 @@ package jsonchange
 
 import (
 	"encoding/json"
+	"io"
+	"io/ioutil"
 )
 
 func JsonToByByte(input []byte, inObj interface{}, outObj interface{}, callback func(in interface{}, out interface{})) ([]byte, error) {
@@ -11,4 +13,12 @@ func JsonToByByte(input []byte, inObj interface{}, outObj interface{}, callback 
 	}
 	callback(inObj, outObj)
 	return json.Marshal(outObj)
+}
+
+func JsonToByIoReader(reader io.Reader, inObj interface{}, outObj interface{}, callback func(in interface{}, out interface{})) ([]byte, error) {
+	b, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return JsonToByByte(b, inObj, outObj, callback)
 }
