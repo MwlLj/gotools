02d14cbe41374abef371ed69dfaab05267dfd748
MwlLj <731025894@qq.com>
2019-05-25 22:20:24 +0800

    新增json相关模块

--- jsonchange/jsonchange.go
+++ jsonchange/jsonchange.go
@@ -15,6 +15,18 @@ func JsonToByByte(input []byte, inObj interface{}, outObj interface{}, callback 
 	return json.Marshal(outObj)
 }
 
+func JsonToByByteWithRet(input []byte, inObj interface{}, outObj interface{}, callback func(in interface{}, out interface{}) error) ([]byte, error) {
+	err := json.Unmarshal(input, inObj)
+	if err != nil {
+		return nil, err
+	}
+	err = callback(inObj, outObj)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(outObj)
+}
+
 func JsonToByIoReader(reader io.Reader, inObj interface{}, outObj interface{}, callback func(in interface{}, out interface{})) ([]byte, error) {
 	b, err := ioutil.ReadAll(reader)
 	if err != nil {
@@ -30,4 +42,12 @@ func JsonToString(inObj interface{}, out *string) error {
 	}
 	*out = string(b)
 	return nil
+}
+
+func JsonFromByte(b []byte, outObj interface{}) error {
+	return json.Unmarshal(b, &outObj)
+}
+
+func JsonFromString(s *string, outObj interface{}) error {
+	return JsonFromByte([]byte(*s), outObj)
 }
