d690cb48aae16241713e68637d9a744664bc2b12
MwlLj <731025894@qq.com>
2019-05-31 14:33:42 +0800

    新增ospath

---
+++ ospath/ospath.go
@@ -0,0 +1,23 @@
+package ospath
+
+import (
+	"os"
+)
+
+func Exists(path *string) bool {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func CreateDirsIfNotExists(path *string) error {
+	if Eixsts(*path) == false {
+		err := os.MkdirAll(*path, 0777)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
